e172bfcc5d20ed51ce0603dc18e785d43abea1b1
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2018-11-24 20:52:16 -0200

    Sat Nov 24 20:52:16 -02 2018

---
+++ .scripts/update_dotfiles.sh
@@ -0,0 +1,3 @@
+cp $HOME/.bash_aliases $HOME/.bash_profile $HOME/.bashrc $HOME/.dir_colors $HOME/.gitconfig $HOME/.vimrc $HOME/Felipe/dotfiles
+cp -r $HOME/.scripts $HOME/Felipe/dotfiles
+cp -r $HOME/.vim $HOME/Felipe/dotfiles
